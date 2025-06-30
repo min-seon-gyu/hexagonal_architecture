9c601bd61bec04fe64250781d140d117b754471d
Seongyu <gcael@naver.com>
2025-06-30 20:20:54 +0900

    project setting

---
+++ src/test/java/study/hexagonal_architecture/HexagonalArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package study.hexagonal_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
